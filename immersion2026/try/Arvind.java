15dbdc02a012d41632e1f414e929afa36c162dcd
Arvind Yadav <arvyadav1975s@gmail.com>
2026-06-24 10:25:34 +0530

    Delete immersion2026/try directory

--- immersion2026/try/Arvind.java
+++
@@ -1,8 +0,0 @@
-//write a program of name whitout using samiclon in java//
-public class Arvind {
-    public static void main(String[] args) {
-        if (System.out.printf("Arvind%nYadav%n")==null) {
-        }
-    }
-}
-//printf return number of characters printed//
