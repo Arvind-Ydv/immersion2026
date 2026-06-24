b839476ceb97ab5a3ffd1829919ac5b7659d2080
Arvind Yadav <arvyadav1975s@gmail.com>
2026-06-24 10:23:54 +0530

    Fix repository structure

---
+++ immersion2026/try/Arvind.java
@@ -0,0 +1,8 @@
+//write a program of name whitout using samiclon in java//
+public class Arvind {
+    public static void main(String[] args) {
+        if (System.out.printf("Arvind%nYadav%n")==null) {
+        }
+    }
+}
+//printf return number of characters printed//
---
+++ immersion2026/try/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello! ");
+        System.out.print("Arvind Yadav");
+
+        
+        
+    }
+}
+
+//what is IS-A and HAS-A relationship in Java?//
+//write a program where there is no use of vowel in the variable name and class name.//
+
+
+//display name without vowel using ASCII//
+
+
+
+
+
